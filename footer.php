fd07797f13039a5464dc4c710402c6bf0bdec192
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-05-13 10:29:05 -0400

    Cambio de documentacion de html a php para inclucion de archivos y creacion de archivos header y footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="rede-sociales">
+
+    </div>
+    <div class="idioma">
+        <span class="en">English</span>
+        <input type="checkbox" class="ckeck">
+        <span class="es">Español</span>
+        <h1>Title</h1>
+        <p>Hellow</p>
+    </div>
+</footer>
+
+<script src="js/languages.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
